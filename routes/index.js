0328c414c02f14652c2c09283773e15f0b437d82
patelritesh261 <patelritesh261@gmail.com>
2016-02-09 17:39:15 -0500

    add css and js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
+var nodemailer = require("nodemailer");
 var router = express.Router();
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "gmail",
+    auth: {
+        user: "patelritesh261@gmail.com",
+        pass: "9558346094"
+    }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,7 +32,26 @@ router.get('/profilepage', function(req, res) {
 
 /* GET services page. */
 router.get('/contactus', function(req, res) {
+    
   res.render('contactus', { title: 'Contact US' });
 });
 
+/* send e-mail*/
+router.get('/send',function(req,res){
+    var mailOptions={
+   to : req.query.to,
+   subject :req.query.subject,
+   text : req.query.text
+}
+console.log(mailOptions);
+smtpTransport.sendMail(mailOptions, function(error, response){
+if(error){
+console.log(error);
+res.end("error");
+}else{
+console.log("Message sent: " + response.message);
+res.end("sent");
+}
+});
+});
 module.exports = router;
